5e8724f45a401c2f1192148a5263cfe0b12818d6
=tzniceguy <=joeshimbi@gmail.com>
2024-02-29 21:28:16 +0300

    intializing the Nav component

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className='nav'>
+        <nav className='bg-gray-500'>
+            
+        </nav>
+    </div>
+  )
+}
