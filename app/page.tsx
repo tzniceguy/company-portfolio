371b3c92c8e547a0b383eec1bd7304760696187c
=tzniceguy <=joeshimbi@gmail.com>
2024-03-04 10:45:41 +0300

    addded the components to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
-
+import Header from "./components/header/page";
+import Services from "./components/services/page";
+import Testimonials
+ from "./components/testimonials/page";
 export default function Home() {
   return (
     <main>
-      <div>Homepage</div>
+      <Header/>
+      <Services/>
+      <Testimonials/>
     </main>
   );
 }
