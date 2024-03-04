f11547877d0c763955c6e704c1d6b7d16cd270a5
=tzniceguy <=joeshimbi@gmail.com>
2024-03-04 10:11:07 +0300

    init the testimonials component

---
+++ app/components/testimonials/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Testimonial from '../testimonial/page'
+
+export default function Testimonials() {
+    const testimonials = [
+        {name: '', title: '', content: '', avatar: '', url:''},
+    ]
+
+
+
+  return (
+    <div>Testimonials</div>
+  )
+}
