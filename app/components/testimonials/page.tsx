9ac6ba43c4570e046d58771c74530ae3552c582a
=tzniceguy <=joeshimbi@gmail.com>
2024-03-04 10:20:29 +0300

    maped through the array to show the testimonials

--- app/components/testimonials/page.tsx
+++ app/components/testimonials/page.tsx
@@ -3,12 +3,44 @@ import Testimonial from '../testimonial/page'
 
 export default function Testimonials() {
     const testimonials = [
-        {name: '', title: '', content: '', avatar: '', url:''},
-    ]
-
+        {
+          name: 'John Doe',
+          title: 'CEO, SinoHydro Construction Company',
+          content: 'This company is amazing! They provided excellent service and exceeded my expectations.',
+          avatar: 'company3.jpg',
+          url: 'https://www.sinohydro.com/',
+        },
+        {
+          name: 'Jane Smith',
+          title: 'Marketing Director, ACME Inc.',
+          content: 'I highly recommend their services. They were professional and delivered high-quality work.',
+          avatar: '/company2.jpg',
+          url: 'https://www.acmeinc.com/',
+        },
+        {
+            name: 'Victor Timely',
+            title: 'Marketing Director, Time Variant Authority.',
+            content: 'I highly recommend their services. They were professional and delivered high-quality work.',
+            avatar: '/company1.jpg',
+            url: 'https://www.acmeinc.com/',
+          },
+        // Add more testimonials as needed
+      ];
+      
 
 
   return (
-    <div>Testimonials</div>
+    <div>
+        {testimonials.map((testimonial, index) => (
+            <Testimonial
+                key={index}
+                title={testimonial.title}
+                name={testimonial.name}
+                content = {testimonial.content}
+                avatar={testimonial.avatar}
+                url={testimonial.url}
+            />
+        ))}
+    </div>
   )
 }
