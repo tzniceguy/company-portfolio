20bae75f2a16f5c602c8806f90231d1e040a2d10
=tzniceguy <=joeshimbi@gmail.com>
2024-03-04 09:45:04 +0300

    added descriptions to services offered

--- app/components/service/page.tsx
+++ app/components/service/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import {
     Card,
     CardContent,
-    CardDescription,
+   
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
--- app/components/services/page.tsx
+++ app/components/services/page.tsx
@@ -4,12 +4,12 @@ import Service from '../service/page';
 export default function Services() {
     const services = [
         { title: 'Facility and Camp Management', image: '/camp-management.jpg', description: '' },
-        { title: 'Catering', image: '/catering-services.jpeg', description: '' },
+        { title: 'Catering', image: '/catering-services.jpeg', description: 'We provide catering services to various events ' },
         { title: 'Laundry Services', image: '/laundry.jpg', description: '' },
-        { title: 'Waste Management', image: '/waste-management.jpeg', description: '' },
+        { title: 'Waste Management', image: '/waste-management.jpeg', description: 'We provide waste and scrape management and disposal in streets of towns, municipalities and cities' },
         { title: 'Gardening And Cleaning', image: '/gardening.jpg', description: '' },
-        { title: 'Mining Supply Tools', image: '/mining-equipments.jpg', description: '' },
-        { title: 'Hardware Supply', image: '/hardware.jpg', description: '' },
+        { title: 'Mining Supply Tools', image: '/mining-equipments.jpg', description: 'We provide hire to mining equipment to companies n need' },
+        { title: 'Hardware Supply', image: '/hardware.jpg', description: 'We provide hardware materials for construction and maintanance services' },
         { title: 'Supply of Construction and Building Materials', image: '/construction.jpg', description: 'We provide construction building materials to companies and individuals in need' },
         { title: 'Machine And Equipment Hire', image: '/equipment-hire.jpg', description: '' },
         { title: 'Mechanical Engineering', image: '/mechanical-engineering.jpg', description: '' },
