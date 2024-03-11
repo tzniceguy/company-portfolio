93f65c7979c8c31a15619f19b74ad2d58a1431fd
=tzniceguy <=joeshimbi@gmail.com>
2024-03-11 10:09:46 +0300

    no idea

--- app/components/testimonial/page.tsx
+++ app/components/testimonial/page.tsx
@@ -2,46 +2,47 @@ import React from 'react'
 import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
 
 type Props= {
-  avatar: string;
+  params: {avatar: string,
   content: string;
-  name: string;
-  title: string;
-  url: string;
+  name: string,
+  title: string,
+  url: string,}
 }
 
 
-export default function Testimonial({avatar, name, title, content, url}:Props) {
+export default function Testimonial({params}:Props) {
   return (
-    <div className='p-4 m-4 rounded shadow-lg hover:shadow-2xl'>
+    <div className='p-4 m-4 rounded shadow-lg hover:shadow-2xl'>{params &&(
         <Card className="max-w-[400px]">
         <CardHeader className="flex gap-3">
             <Image
-            alt={name}
+            alt={params.name}
             height={40}
             radius="sm"
-            src={avatar}
+            src={params.avatar}
             width={40}
             />
             <div className="flex flex-col">
-            <p className="text-md">{name}</p>
-            <p className="text-small text-default-500">{title}</p>
+            <p className="text-md">{params.name}</p>
+            <p className="text-small text-default-500">{params.title}</p>
             </div>
         </CardHeader>
         <Divider/>
         <CardBody>
-            <p>{content}</p>
+            <p>{params.content}</p>
         </CardBody>
         <Divider/>
         <CardFooter>
             <Link
             isExternal
             showAnchorIcon
-            href={url}
+            href={params.url}
             >
             Visit.
             </Link>
         </CardFooter>
         </Card>
+        )}
     </div>
   );
 }
